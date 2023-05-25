4b4295b324bd616a39a36faf7a340bed13505b65
Cesarin Garcia <cesaremiliogagu@outlook.com>
2023-05-24 20:36:16 -0600

    cambios del CSS

--- administradores.php
+++ administradores.php
@@ -5,28 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administradores</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="central-style.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
+    <nav role="navigation">
+        <div id="menuToggle">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <a href="formulario.php"><li>Cerrar Sesion</li></a>
+                <a href="mostrar_datos1.php"><li>Registros Actuales</li></a>
+                <a href="historial.php"><li>Historial</li></a>
+                <a href="inventario.php"><li>Inventario</li></a>
+                <a href="administradores.php"><li>Administradores</li></a>
+            </ul>
+        </div>
+    </nav>
 
 <?php
     $conexion = mysqli_connect("localhost", "root", "administrador", "inventarioc");
     //Conexión con la tabla "admins" en la BDD
     $comando = "SELECT * FROM admins";
     $resultado = mysqli_query($conexion, $comando);
-    $validacion = mysqli_fetch_array($resultado);
-    echo '<nav class="menu">
-            <div class="contenedor">
-            <ul> 
-                <li><a href="formulario.php">CERRAR SESION</a></li>
-                <li><a href="mostrar_datos1.php">REGISTROS ACTUALES</a></li>
-                <li><a href="historial.php">HISTORIAL</a></li>
-                <li><a href="inventario.php">INVENTARIO</a></li>
-                <li><a href="administradores.php">ADMINISTRADORES</a></li>
-            </ul>
-            </div>
-          </nav>';  
+    $validacion = mysqli_fetch_array($resultado); 
         echo "
         <br><h1><center>Control de administradores</center></h1>";
         echo "Usuario: ".$validacion['nombre'];
@@ -36,7 +40,7 @@
         $resultado = mysqli_query($conexion, $querySelect);
 ?>
     <!--La tabla principal al entrar en admins-->
-        <table> 
+        <table id="container"> 
             <tr>
                <th>ID</th>
                <th>Nombre Completo</th>
